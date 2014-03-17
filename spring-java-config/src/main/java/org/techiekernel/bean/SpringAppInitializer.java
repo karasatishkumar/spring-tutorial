7763e638c83328d0aa1e8e2a594a75e4ef371594
karasatishkumar <karasatishkumar@gmail.com>
2014-03-17 21:54:21 +0530

    anotation application config changes

--- spring-java-config/src/main/java/org/techiekernel/bean/SpringAppInitializer.java
+++ spring-java-config/src/main/java/org/techiekernel/bean/SpringAppInitializer.java
@@ -3,15 +3,11 @@ package org.techiekernel.bean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+
 public class SpringAppInitializer {
-	
 	public static void main(String[] args) {
-		
-		ApplicationContext springContext = new AnnotationConfigApplicationContext("org.techiekernel.bean");
-		
-		
-		HelloWorld helloWorld = (HelloWorld)springContext.getBean(HelloWorld.class);
-		
+		ApplicationContext context = new AnnotationConfigApplicationContext(HelloWorld.class, Foo.class, Bar.class);
+		HelloWorld helloWorld = context.getBean(HelloWorld.class);
 		System.out.println(helloWorld);
 	}
 }
